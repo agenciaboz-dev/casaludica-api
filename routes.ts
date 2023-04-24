2f97f1ee72db7c45c524cbcc024cd0145931b900
nandobfer <nandobfer@yahoo.com.br>
2023-04-24 09:42:05 -0300

    feat: added products route and get endpoint

--- routes.ts
+++ routes.ts
@@ -1,6 +1,7 @@
 import express, { Express, Request, Response } from 'express'
 import login from './src/login'
+import products from './src/products'
 
 export const router = express.Router()
 
-router.use('/login', login)
+router.use('/products', products)
