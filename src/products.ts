0407458cef49cc632221f037108548999061519d
nandobfer <nandobfer@yahoo.com.br>
2023-04-25 12:23:29 -0300

    update: added query order

--- src/products.ts
+++ src/products.ts
@@ -5,7 +5,7 @@ const prisma = new PrismaClient()
 
 router.get('/', async (request:Request, response:Response) => {    
 
-    const products = await prisma.products.findMany({ include: { categories: { include: { colections: true } } } })    
+    const products = await prisma.products.findMany({ include: { categories: { include: { colections: true } } }, orderBy: { featured: 'desc' } })    
     response.json(products)
 })
 
@@ -13,7 +13,7 @@ router.post('/', async (request:Request, response:Response) => {
     const data = request.body
     console.log(data)
 
-    const products = await prisma.products.findMany({ where: { name: { search: data.search } }, include: { categories: { include: { colections: true } } } })    
+    const products = await prisma.products.findMany({ where: { name: { search: data.search } }, include: { categories: { include: { colections: true } } }, orderBy: { featured: 'desc' } })    
     response.json(products)
 })
 
@@ -21,7 +21,7 @@ router.post('/category', async (request:Request, response:Response) => {
     const data = request.body
     console.log(data)
 
-    const products = await prisma.products.findMany({ where: { category: data.id }, include: { categories: { include: { colections: true } } } })    
+    const products = await prisma.products.findMany({ where: { category: data.id }, include: { categories: { include: { colections: true } } }, orderBy: { featured: 'desc' } })    
     console.log(products)
     response.json(products)
 
@@ -31,8 +31,14 @@ router.post('/collection', async (request:Request, response:Response) => {
     const data = request.body
     console.log(data)
 
-    const products = await prisma.products.findMany({ where: { category: { in: data.categories } }, include: { categories: { include: { colections: true } } } })
+    const products = await prisma.products.findMany({ where: { category: { in: data.categories } }, include: { categories: { include: { colections: true } } }, orderBy: { featured: 'desc' } })
     console.log(products)
+    response.json(products)
+})
+
+router.get('/popular', async (request:Request, response:Response) => {    
+
+    const products = await prisma.products.findMany({ orderBy: [{ sold: 'desc' }, { featured: 'desc' }], take: 5, include: { categories: { include: { colections: true } } } })
     response.json(products)
 })
 
