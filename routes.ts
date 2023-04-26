1b25a5559347c33153dd33b9cb4770fac3a1199a
nandobfer <nandobfer@yahoo.com.br>
2023-04-26 13:16:34 -0300

    feat: added cep search api

--- routes.ts
+++ routes.ts
@@ -2,8 +2,10 @@ import express, { Express, Request, Response } from 'express'
 import login from './src/login'
 import products from './src/products'
 import categories from './src/categories'
+import cep from "./src/cep"
 
 export const router = express.Router()
 
-router.use('/products', products)
-router.use('/categories', categories)
+router.use("/products", products)
+router.use("/categories", categories)
+router.use("/cep", cep)
