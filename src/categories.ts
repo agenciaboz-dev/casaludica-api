b17ec1ed222049e5bbfca613277428948330048e
nandobfer <nandobfer@yahoo.com.br>
2023-09-06 17:13:41 -0300

    update: getting categories from igesp

--- src/categories.ts
+++ src/categories.ts
@@ -1,12 +1,20 @@
 import express, { Express, Request, Response } from 'express'
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient, categories } from "@prisma/client"
+import api from "./api"
 const router = express.Router()
 const prisma = new PrismaClient()
 
-router.get('/', async (request:Request, response:Response) => {    
-    const categories = await prisma.categories.findMany()    
-    
-    response.json(categories)
+router.get("/", async (request: Request, response: Response) => {
+    try {
+        const igestCategories = await api.get.categories({})
+        const categories: categories[] = igestCategories.map((category) => ({ colection: 1, id: category.IdGrupo, name: category.Descricao }))
+
+        console.log(categories)
+        response.json(categories)
+    } catch (error) {
+        console.log(error)
+        response.json([])
+    }
 })
 
 export default router
