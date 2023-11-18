ca75d93d675c98c4d30858fd766588e089d9fd81
nandobfer <nandobfer@yahoo.com.br>
2023-11-18 11:09:32 -0300

    feat: added build script

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+api="casaludica-api"
+user="burgos"
+path="~/${api}/"
+
+echo 'compiling server filies locally'
+yarn build
+
+echo 'Uploading build to server'
+scp -r dist/* ${user}@agencyboz:${path}
+
+echo 'restarting server api'
+ssh ${user}@agencyboz "source ~/.nvm/nvm.sh; pm2 restart ${api}"
