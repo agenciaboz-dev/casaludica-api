8052051ecdad70cc3055bd352096eefc3e0501dd
ispadilha <israel.padilha@gmail.com>
2024-03-11 16:59:33 -0300

    update: password generation e-mail

--- src/templates/email/generate_password.ts
+++ src/templates/email/generate_password.ts
@@ -1,1 +1,15 @@
-export const generate_password = (url: string) => `<div><p>clica aí pra gerar a senha: </p><a href="${url}">gerar senha</a></div>`
+export const generate_password = (url: string) => `
+    <table cellpadding="0" cellspacing="0" border="0">
+        <tr>
+            <td>
+                <h3 style="color: #363775;">Casa Lúdica - Loja</h3>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <p>Clique no link para gerar sua senha:</p>
+                <a href="${url}">Gerar senha</a>
+            </td>
+        </tr>
+    </table>
+`
