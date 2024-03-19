0cc52fc69d6c13cf00f0a409401cfbc3be9969f0
nandobfer <nandobfer@yahoo.com.br>
2024-03-19 09:09:59 -0300

    update: igest version

--- src/api/igest.ts
+++ src/api/igest.ts
@@ -2,7 +2,7 @@ import axios from "axios"
 import { IgestNewOrder } from "../types/igest/Order"
 
 const token = "9aB4pC!qRt3xYz"
-const version = 6
+const version = 7
 
 const api = axios.create({
     // baseURL: "http://localhost:4100/api",
