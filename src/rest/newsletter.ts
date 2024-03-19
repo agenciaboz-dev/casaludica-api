33858d7879753fdc2a5ac6cfe0614f78b1a593a2
nandobfer <nandobfer@yahoo.com.br>
2024-03-19 17:35:22 -0300

    update: sending mail on newsletter

--- src/rest/newsletter.ts
+++ src/rest/newsletter.ts
@@ -1,11 +1,13 @@
 import express, { Express, Request, Response } from "express"
 import { PrismaClient } from "@prisma/client"
+import { sendMail } from "../tools/mail"
 const router = express.Router()
 const prisma = new PrismaClient()
 
 router.post("/", async (request: Request, response: Response) => {
     const data = request.body as { name: string; email: string }
     const newsletter = await prisma.newsletter.create({ data })
+    await sendMail(newsletter.email, "assinado newsletter", "email texto", "<p>newsletter html</p>")
     response.status(200)
 })
 
