6e9e65c60a700bcd10aef40ebf3450860c8bd174
nandobfer <nandobfer@yahoo.com.br>
2024-04-03 15:00:55 -0300

    feat: getting instagram images

--- src/rest/instagram.ts
+++ src/rest/instagram.ts
@@ -1,11 +1,33 @@
 import express, { Express, Request, Response } from "express"
+import playwright from "playwright"
+
 const router = express.Router()
 
-router.get("/authorize", async (request: Request, response: Response) => {
-    const code = request.query.code
-    console.log(code)
-
-    response.send(code)
+router.get("/scrape", async (request: Request, response: Response) => {
+    const url = `https://instagram.com/casaludica`
+    console.log("stating browser to get instagram posts")
+    const browser = await playwright["chromium"].launch()
+    try {
+        const context = await browser.newContext()
+        const page = await context.newPage()
+        await page.goto(url)
+        await page.waitForSelector("._aagv > img", {
+            state: "visible",
+        })
+        await page.screenshot({ path: `profile.png` })
+        const data = await page.evaluate(() => {
+            const images = document.querySelectorAll("._aagv > img")
+            const urls = Array.from(images).map((v) => (v as HTMLImageElement).src)
+            return urls
+        })
+        console.log(data)
+        console.log(data.length)
+        response.json(data)
+    } catch (error) {
+        console.log(error)
+    } finally {
+        await browser.close()
+    }
 })
 
 export default router
